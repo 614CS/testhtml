3a9d8486b88edd45aa287bd007017c851dc2012a
614CS <alejandrosoriano@gmail.com>
2026-04-08 10:55:38 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,18 +1,3 @@
+console.log("Hello, World!");
 
- //<!-- invocar js de supabase -->   
-  src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.101.1/dist/umd/supabase.min.js"
-    
-   
 
-  const supabaseUrl = "https://tszvowudspzdoqbemruw.supabase.co"
-  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzenZvd3Vkc3B6ZG9xYmVtcnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNTM5NDksImV4cCI6MjA5MDkyOTk0OX0.iEck81nx9OuQlOS6OLX7L0a0cqswkmzMKISMhvSFDgI"
-  const supabasePublicClient = supabase.createClient(supabaseUrl, supabaseKey);
-
-  async function getData() {      
-    const { data, error } = await supabasePublicClient   
-    .from('test')
-    .select('*') // Select all columns 
-    if (error) console.error('Error getting data:', error)
-    else console.log(JSON.stringify(data, null, 2))
-    }  
-      
