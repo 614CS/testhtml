1f803326113224def535ad73f89fcf48c7d63a82
614CS <alejandrosoriano@gmail.com>
2026-04-08 21:06:26 -0600

    Update and rename index.html to worker.js

---
+++ worker.js
@@ -0,0 +1,15 @@
+				const supabaseUrl = "https://tszvowudspzdoqbemruw.supabase.co"
+				const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzenZvd3Vkc3B6ZG9xYmVtcnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNTM5NDksImV4cCI6MjA5MDkyOTk0OX0.iEck81nx9OuQlOS6OLX7L0a0cqswkmzMKISMhvSFDgI"
+				const supabasePublicClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+				async function getData() {
+                    const { data: records, error } = await supabasePublicClient      
+					.from('test').select('*') // Select all columns
+					if (error) {
+						console.error('Error fetching data:', error.message)
+						return
+					}
+  console.log('Fetched Data:', records)
+}
+
+fetchData()
