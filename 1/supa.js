eb83dcd3b2659ae2418798d75f84c79d0d95e3d6
614CS <alejandrosoriano@gmail.com>
2026-04-10 01:47:27 -0600

    Update supa.js

--- 1/supa.js
+++ 1/supa.js
@@ -1,5 +1,5 @@
 
-const DATA_API_URL = 'https://ep-weathered-haze-akbm51ki.apirest.c-3.us-west-2.aws.neon.tech/neondb/rest/v1';
+const DATA_API_URL = 'https://ep-weathered-haze-akbm51ki.neonauth.c-3.us-west-2.aws.neon.tech/neondb/auth/.well-known/jwks.json';
 const TABLE_NAME = 'test';
 
    fetch(`${DATA_API_URL}/${TABLE_NAME}`)
