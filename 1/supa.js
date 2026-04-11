4a23a7e18a94d90a49cc8e7d781b93cf0a7aeba6
614CS <alejandrosoriano@gmail.com>
2026-04-11 16:07:50 -0600

    Update supa.js

--- 1/supa.js
+++ 1/supa.js
@@ -1,1 +1,19 @@
+const DATA_API_URL = "https://ep-still-grass-a88vqwik.apirest.eastus2.azure.neon.tech/neondb/rest/v1";
+const TABLE_NAME = "test";
 
+fetch(`${DATA_API_URL}/${TABLE_NAME}`, {
+  method: 'GET',
+  headers: {
+        'Accept': 'application/json'
+      }
+
+})
+.then(response => {
+  if (!response.ok) throw new Error(`Error: ${response.status} ${response.statusText}`);
+  return response.json();
+})
+.then(data => {
+  console.log(`Datos de la tabla "${TABLE_NAME}":`);
+  console.table(data); // Muestra los datos en formato de tabla visual en la consola
+})
+.catch(error => console.error('Error al consultar Neon:', error));
