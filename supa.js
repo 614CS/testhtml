f4d10d974f883d3043e28604386f96943ae5b748
614CS <alejandrosoriano@gmail.com>
2026-04-11 23:56:18 -0600

    Update supa.js

--- supa.js
+++ supa.js
@@ -1,3 +1,4 @@
+                import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.101.1/dist/umd/supabase.min.js' 
                 export { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.101.1/dist/umd/supabase.min.js'  //nose pueden dos scripts?
                 //import { createClient } from '@supabase/supabase-js'
                 const supabaseUrl = "https://tszvowudspzdoqbemruw.supabase.co"
