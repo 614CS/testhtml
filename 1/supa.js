c57a6bae92ef6879efe71cf87e9d1356da4d7b3c
614CS <alejandrosoriano@gmail.com>
2026-04-11 20:58:18 -0600

    Update supa.js

--- 1/supa.js
+++ 1/supa.js
@@ -1,4 +1,6 @@
-const DATA_API_URL = "https://ep-still-grass-a88vqwik.apirest.eastus2.azure.neon.tech/neondb/rest/v1";
+//const DATA_API_URL = "https://ep-still-grass-a88vqwik.apirest.eastus2.azure.neon.tech/neondb/rest/v1";
+const DATA_API_URL = "https://ep-divine-haze-akvab75y.apirest.c-3.us-west-2.aws.neon.tech/neondb/rest/v1";
+
 const TABLE_NAME = "test";
 
 fetch(`${DATA_API_URL}/${TABLE_NAME}`, {
