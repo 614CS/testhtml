cba9d337176c6c0c18083397d4c8f61598b5b89e
614CS <alejandrosoriano@gmail.com>
2026-04-12 01:31:09 -0600

    Update supa.js

--- 1/supa.js
+++ 1/supa.js
@@ -1,1 +1,17 @@
+const { createClient } = libsql;
 
+const client = createClient({
+  url: "libsql://test-junyu.aws-us-east-2.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3NzYwNTM0NzYsImlhdCI6MTc3NTk2NzA3NiwiaWQiOiIwMTlkN2UyOS1iZDAxLTcxYTUtODE0Ni02ZDNmYjQyMzY2ZjEiLCJyaWQiOiI1NjUyZjY3NC03Y2E1LTRhYzQtODY0OC1kMDFkMzdmOWExMDYifQ.wSXrCodXMYmXYxyALcRs5KlS5RYOUw8-npoO-m_fZZmcX10eIU8o2-7b8B9CWRz0pHetNDkblleIUDbWhzzQAw"
+});
+
+async function showTable() {
+  try {
+    const rs = await client.execute("SELECT * FROM test");
+    console.table(rs.rows); // Using console.table displays it as a formatted table
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+showTable();
