67028ea02e06a9814ae8b6236d7326ba6a4a8543
614CS <alejandrosoriano@gmail.com>
2026-04-12 10:46:48 -0600

    Update neon12.js

--- 3/neon12.js
+++ 3/neon12.js
@@ -4,7 +4,8 @@ import { neon } from 'https://cdn.jsdelivr.net/npm/@neondatabase/serverless@1.0.
 //const authToken = getAuthToken();
 
 // Initialize the Neon client with connection string and auth token
-const sql = neon('postgresql://anonymous@ep-divine-haze-akvab75y-pooler.c-3.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+const ANONYMIZED_DATABASE_URL = "postgresql://anonymous@ep-divine-haze-akvab75y.c-3.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+const sql = neon(process.env.ANONYMIZED_DATABASE_URL);
 
 // Run a query
 const posts = await sql('SELECT * FROM test');
