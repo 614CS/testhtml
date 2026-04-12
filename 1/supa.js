a9de6b1f012c333454e993c3b806b3bf07c9c055
614CS <alejandrosoriano@gmail.com>
2026-04-11 23:26:34 -0600

    Update supa.js

--- 1/supa.js
+++ 1/supa.js
@@ -1,5 +1,3 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@libsql/client@0.17.2/+esm'; 
-       
 const client = createClient({
   url: "libsql://test-junyu.aws-us-east-2.turso.io",
   authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3NzYwNTM0NzYsImlhdCI6MTc3NTk2NzA3NiwiaWQiOiIwMTlkN2UyOS1iZDAxLTcxYTUtODE0Ni02ZDNmYjQyMzY2ZjEiLCJyaWQiOiI1NjUyZjY3NC03Y2E1LTRhYzQtODY0OC1kMDFkMzdmOWExMDYifQ.wSXrCodXMYmXYxyALcRs5KlS5RYOUw8-npoO-m_fZZmcX10eIU8o2-7b8B9CWRz0pHetNDkblleIUDbWhzzQAw"
